87a0aaa0120f54bfe9e0bd9ef14cc80cb89b7f8e
LuoChen <luochen1990@gmail.com>
2015-05-29 19:04:02 +0800

    new: `streak`, `streak'`, `lastN`

---
+++ streak-and-lastN.hs
@@ -0,0 +1,10 @@
+module Streak (streak, streak', lastN) where
+
+import Data.List (tails)
+
+streak n = filter ((== n) . length) . map (take n) . tails
+
+streak' n xs = streak n $ xs ++ take (n - 1) xs
+
+lastN n = last . streak n
+
