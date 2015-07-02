66268d93ccc95aa10eb6deaed41ef13261918bfd
LuoChen <luochen1990@gmail.com>
2015-07-02 13:28:47 +0800

    [update] streak implementation updated.

--- streak-and-lastN.hs
+++ streak-and-lastN.hs
@@ -2,7 +2,7 @@ module Streak (streak, streak', lastN) where
 
 import Data.List (tails)
 
-streak n = filter ((== n) . length) . map (take n) . tails
+streak n = takeWhile ((== n) . length) . map (take n) . tails
 
 streak' n xs = streak n $ xs ++ take (n - 1) xs
 
